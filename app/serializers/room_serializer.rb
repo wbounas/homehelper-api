f3d8ce8b94112c6ee57a085c6a092da3de8a42ae
wbounas <wbounas@gmail.com>
2018-03-08 16:24:32 -0500

    Edit `RoomSerializer` to serialize out :tasks as an array of IDs

--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -1,5 +1,9 @@
 class RoomSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :size, :notes, :completed, :priority
+  attributes :id, :name, :location, :size, :notes, :completed, :priority, :tasks
   has_many :tasks
   has_one :user
+
+  def tasks
+    object.tasks.map { |task| task.id }
+  end
 end
